e75d29758a784cee738fd2f360f542f40cd5e4e3
unknown <chunkz.ring@gmail.com>
2024-05-27 09:52:35 +0700

    feat: add db integration for edit handler

--- bin/modules/product/handlers/api_handler.go
+++ bin/modules/product/handlers/api_handler.go
@@ -49,6 +49,36 @@ func AddProduct(c *fiber.Ctx) error {
 }
 
 func EditProduct(c *fiber.Ctx) error {
+	var err error
+	var db *sqlx.DB
+	db = postgresqlHelper.GetDB()
+
+	product := Product{}
+	sqlStatement := `SELECT productid, label FROM products WHERE productid=$1 LIMIT 1`
+  rows, _ := db.Queryx(sqlStatement, c.Params("id"))
+	for rows.Next() {
+		err := rows.StructScan(&product)
+		if err != nil {
+				log.Fatalln(err)
+		}
+	}
+	if product.ProductId == "" {
+		return wrapperHelper.Response(c, "fail", nil, "Product not found", 404)
+	}
+
+	p := new(Product)
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+	sqlStatementUpdate := `
+	UPDATE products 
+	SET label = $2, updated_at = $3
+	WHERE productid = $1`
+	_, err = db.Exec(sqlStatementUpdate, c.Params("id"), p.Label, time.Now())
+	if err != nil {
+		panic(err)
+	}
+	
 	return wrapperHelper.Response(c, "default", nil, "Product #" + c.Params("id") + " has been edited", 200)
 }
 
